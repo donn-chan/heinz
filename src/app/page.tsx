bc3e7ac27746fc383e853b455571f1377f5d47b5
donn-neighbors <donn@theneighbors.co>
2025-09-10 22:47:13 +0700

    Added icons to the buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@
 import Image from "next/image";
 import { useState, useRef } from "react";
 import { toPng } from "html-to-image";
+import { Download, Share2 } from "lucide-react";
+
 
 
 export default function Home() {
@@ -147,12 +149,14 @@ export default function Home() {
             onClick={downloadImage}
             className="inline-flex items-center justify-center px-6 py-2 rounded-[10px] bg-black/60 text-white font-bold hover:bg-black/80 transition cursor-pointer"
           >
+            <Download size={18} className="mr-2" />
             Download
           </button>
           <button
             onClick={shareImage}
             className="inline-flex items-center justify-center px-6 py-2 rounded-[10px] bg-black/60 text-white font-bold hover:bg-black/80 transition cursor-pointer"
           >
+            <Share2 size={18} className="mr-2" />
             Share
           </button>
         </div>
