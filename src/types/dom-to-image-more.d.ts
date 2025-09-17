220e32d5ecb0215f957f199339dc55014dac6518
donn-chan <donn.regalado@gmail.com>
2025-09-17 16:12:11 +0700

    added config

--- src/types/dom-to-image-more.d.ts
+++ src/types/dom-to-image-more.d.ts
@@ -1,7 +1,7 @@
 declare module "dom-to-image-more" {
-    export function toPng(node: HTMLElement, options?: any): Promise<string>;
-    export function toJpeg(node: HTMLElement, options?: any): Promise<string>;
-    export function toBlob(node: HTMLElement, options?: any): Promise<Blob>;
-    export function toSvg(node: HTMLElement, options?: any): Promise<string>;
+    export function toPng(node: HTMLElement, options?: unknown): Promise<string>;
+    export function toJpeg(node: HTMLElement, options?: unknown): Promise<string>;
+    export function toBlob(node: HTMLElement, options?: unknown): Promise<Blob>;
+    export function toSvg(node: HTMLElement, options?: unknown): Promise<string>;
   }
   
